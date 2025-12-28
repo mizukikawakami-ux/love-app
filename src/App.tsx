b49c9dbba576b7b8ec0cd633e50cbed2e725bb28
mizukikawakami-ux <mizuki.kawakami@sukima-ad.com>
2025-12-28 15:38:25 +0900

    Upload for Vercel

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Analytics } from "@vercel/analytics/react";
 import Home from './pages/Home';
 import Question from './pages/Question';
 import Analysis from './pages/Analysis';
@@ -17,6 +18,7 @@ function App() {
         <Route path="/advice" element={<Advice />} />
         <Route path="/premium" element={<Premium />} />
       </Routes>
+      <Analytics />
     </BrowserRouter>
   );
 }
